a30b634f9a60ccd4ab6decc78db71ccc423d8368
Benjamin Kahn <bkahn@redhat.com>
2021-06-03 11:30:11 -0400

    New library file which defines types of object This is an experiment; expect some changes to this file

---
+++ pyigloo/iglootypes.py
@@ -0,0 +1,43 @@
+types = {
+    # Containers
+    "Community":
+        {"children": True,
+        "content": False},
+    "Space":
+        {"children": True,
+        "content": False},
+    "Page":
+        {"children": True,
+        "content": False},
+
+    # Content Channels
+    "Wiki":
+        {"children": True,
+        "content": True},
+    "BlogChannel":
+        {"children": True,
+        "content": True},
+    "FolderChannel":
+        {"children": True,
+        "content": True},
+    "ForumChannel":
+        {"children": True,
+        "content": True},
+    "Calendar":
+        {"children": True,
+        "content": True},
+        
+    # Content
+    "WikiArticle":
+        {"children": False,
+        "content": True},
+    "BlogArticle":
+        {"children": False,
+        "content": True},
+
+    # Search Results
+    "IglooList":
+        {"children": True,
+        "content": False}
+
+}
