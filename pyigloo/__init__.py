a6cea14d3df52195a5a28db11293b2a7b73b3fa8
Benjamin Kahn <bkahn@redhat.com>
2021-06-03 11:27:41 -0400

    Add two new API calls - community_usergroups_view -- get information about a usergroup - apisync_view_usergroups -- get groups from a list of members
    Also fixed doc URLs for a number of API calls

--- pyigloo/__init__.py
+++ pyigloo/__init__.py
@@ -55,7 +55,7 @@ class igloo:
         """
         APIv1 session/create
 
-        https://source.redhat.com/cmedia/api-docs/#/Session/post__api_api_svc_session_create
+        https://customercare.igloosoftware.com/cmedia/api-docs/#/Session/post__api_api_svc_session_create
         """
         url = '{0}{1}session/create'.format(self.endpoint, self.IGLOO_API_ROOT_V1)
         headers = {b'Accept': 'application/json'}
@@ -66,7 +66,7 @@ class igloo:
         """
         APIv2 Session/Create
 
-        https://source.redhat.com/cmedia/api-docs/?api=api2#/Session/Session_CreateV1
+        https://customercare.igloosoftware.com/cmedia/api-docs/?api=api2#/Session/Session_CreateV1
         """
         url = '{0}{1}Session/Create'.format(self.endpoint, self.IGLOO_API_ROOT_V2)
         result = self.igloo.post(url, json=params)
@@ -108,7 +108,7 @@ class igloo:
         """
         APIv1 objects/byPath call
 
-        https://source.redhat.com/cmedia/api-docs/#/Objects/get__api_api_svc_objects_byPath
+        https://customercare.igloosoftware.com/cmedia/api-docs/#/Objects/get__api_api_svc_objects_byPath
         Given a URI fragment, return information about the object
 
         This is the most common API call, allowing you to dereference a URL to an ID
@@ -116,4 +116,28 @@ class igloo:
         url = '{0}{1}/objects/byPath'.format(self.endpoint, self.IGLOO_API_ROOT_V1)
         headers = {b'Accept': 'application/json'}
         result = self.igloo.get(url, headers=headers, params={'path': path, 'domain': domain})
+        return result.json()['response']
+
+    def apisync_view_usergroups (self, userIds = []):
+        """
+        APIv1 apisync/view_usergroups call
+        
+        https://customercare.igloosoftware.com/cmedia/api-docs/#/APISync/post__api_api_svc_apisync_view_usergroups
+        Return a list of group IDs a user belongs to
+        """
+        url = '{0}{1}/apisync/view_usergroups'.format(self.endpoint, self.IGLOO_API_ROOT_V1)
+        payload = {"userIds": userIds}
+        headers =  {b'Accept': 'application/json'}
+        result = self.igloo.post(url, headers=headers, params=payload)
+        return result.json()['dictionary']
+
+    def community_usergroups_view (self, usergroupId):
+        """
+        APIv1 /community/usergroups/{usergroupId}/view call
+
+        https://customercare.igloosoftware.com/cmedia/api-docs/#/CommunityUserGroups/get__api_api_svc_community_usergroups__usergroupId__view
+        """
+        url = '{0}{1}/community/usergroups/{2}/view'.format(self.endpoint, self.IGLOO_API_ROOT_V1, usergroupId)
+        headers =  {b'Accept': 'application/json'}
+        result = self.igloo.get(url, headers=headers)
         return result.json()['response']
