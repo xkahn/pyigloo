13002d7cb9ec7906546cc2991126b7a8938e542c
Benjamin Kahn <bkahn@redhat.com>
2022-03-16 12:59:43 -0400

    New class to support Igloo API date formats

---
+++ pyigloo/igloodates.py
@@ -0,0 +1,22 @@
+import re
+from datetime import datetime
+from dateutil.tz import tzoffset
+
+"""
+Igloo dates are formatted like: /Date(1646832169387-0500)/
+The format is /Date( TIMESTAMP TIMEZONE )
+"""
+
+class date:
+
+    def __init__ (self, timestamp):
+        self.raw = timestamp
+        if m := re.match(r"\/Date\(([0-9]+)([+-][0-9]{4})\)", timestamp):
+            self.timestamp = m.group(1)
+            self.timezone = m.group(2)
+            d = datetime.fromtimestamp(int(self.timestamp)/1000)
+            self.datetime = datetime(d.year, d.month, d.day, d.hour, d.minute, d.second, d.microsecond, tzinfo=tzoffset(None, int(m.group(2))*6*6))
+
+    def __str__(self):
+        return self.datetime.strftime("%c")
+
