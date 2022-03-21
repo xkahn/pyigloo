a5461e39c9bfd7d25c9a7c253d6e1518ca0c7df5
Benjamin Kahn <bkahn@redhat.com>
2022-03-21 13:40:31 -0400

    Add sorting columns (ugh)

--- tests/inventorylist.py
+++ tests/inventorylist.py
@@ -26,13 +26,15 @@ import xlsxwriter
 class file_writer:
     levels = {}
 
-    def __init__(self, workbook=None, csv=None):
+    def __init__(self, workbook=None, csv=None, doGroup=False):
         self.row = 0
         self.workbook = workbook
+        self.doGroup = doGroup
         if workbook:
                 self.worksheet = workbook.add_worksheet()
         self.csv = csv
         if workbook:
+            self.worksheet.freeze_panes(1, 0)
             self.levels[0] = self.workbook.add_format()
             self.levels[1] = self.workbook.add_format()
             self.levels[2] = self.workbook.add_format()
@@ -40,8 +42,10 @@ class file_writer:
             self.levels[4] = self.workbook.add_format()
             self.levels[0].set_font_name('Red Hat Text')
             self.levels[1].set_indent(1)
+            self.levels[1].set_num_format("   >> @")
             self.levels[1].set_font_name('Red Hat Text')
             self.levels[2].set_indent(2)
+            self.levels[2].set_num_format("      -- @")
             self.levels[2].set_font_name('Red Hat Text')
             self.levels[3].set_font_color("#777777")
             self.levels[3].set_font_name('Red Hat Text')
@@ -49,20 +53,22 @@ class file_writer:
             self.levels[4].set_font_name('Courier New')
             self.worksheet.set_column(0, 0, 40)
             self.worksheet.set_column(1, 1, 40)
-            self.worksheet.set_column(2, 3, 15)
-            self.worksheet.set_column(4, 4, 15)
-            self.worksheet.set_column(6, 7, 25)
+            self.worksheet.set_column(2, 7, 15)
+            self.worksheet.set_column(8, 8, 15)
+            self.worksheet.set_column(10, 11, 25)
 
     def writerow (self, data, indent=0, outline=0, collapse=False):
         if self.worksheet:
             self.worksheet.write_row(self.row, 0, data, self.levels[0])
             if self.row > 0:
                 self.worksheet.write(self.row, 1, data[1], self.levels[3])
-            if type(data[2]) == datetime:
-                self.worksheet.write_datetime(self.row, 2, data[2], self.levels[4])
-            if type(data[3]) == datetime:
-                self.worksheet.write_datetime(self.row, 3, data[3], self.levels[4])
-            if outline > 0:
+            self.write_datevalue(data, 2)
+            self.write_datevalue(data, 3)
+            self.write_datevalue(data, 4)
+            self.write_datevalue(data, 5)
+            self.write_datevalue(data, 6)
+            self.write_datevalue(data, 7)
+            if self.doGroup and outline > 0:
                 self.worksheet.set_row(self.row, None, None, {'level': outline, 'hidden': collapse})
             if indent > 0:
                 self.worksheet.write(self.row, 0, data[0], self.levels[indent])
@@ -71,23 +77,32 @@ class file_writer:
             self.csv.writerow(data)
         self.row = self.row+1
 
+    def write_datevalue (self, data, column):
+        if type(data[column]) == datetime:
+            self.worksheet.write_datetime(self.row, column, data[column], self.levels[4])
 
-def display_container (writer, containerid, tz):
+
+def display_container (writer, containerid, tz, ):
     info = igloo.objects_view(containerid)
     writer.writerow([
         '=HYPERLINK("' + os.getenv("API_ENDPOINT")[:-1] + info["href"] + '","' + info["title"] + '")',
         info["href"],
         pyigloo.igloodates.date(info["created"]["date"], tz).local,
+        None, # article created
+        None, # comment created
         pyigloo.igloodates.date(info["modified"]["date"], tz).local,
+        None, # article updated
+        None, # comment updated
         pyigloo.iglootypes.types(info).__str__(),
         None,
         get_userinfo(info["created"]),
         get_userinfo(info["modified"]),
         info["statistics"]["views"]["views"],
+        None, # article views
         None,
         None,
-        None,
-        info["statistics"]["contents"]["children"]
+        info["statistics"]["contents"]["children"],
+        None # Notes
     ])
 
     children = igloo.get_all_children_from_object(containerid)
@@ -104,24 +119,33 @@ def display_channel (writer, channelid, tz):
         '=HYPERLINK("' + os.getenv("API_ENDPOINT")[:-1] + info["href"] + '","' + info["title"] + '")',
         info["href"],
         pyigloo.igloodates.date(info["created"]["date"], tz).local,
+        None, # article created
+        None, # comment created
         pyigloo.igloodates.date(info["modified"]["date"], tz).local,
-        pyigloo.iglootypes.types(info).__str__(),
+        None, # article updated
+        None, # comment updated
+         pyigloo.iglootypes.types(info).__str__(),
         None,
         get_userinfo(info["created"]),
         get_userinfo(info["modified"]),
         info["statistics"]["views"]["views"],
+        None, #article views
         info.get("version"),
         info.get("numAttachments"),
         info["statistics"]["contents"]["comments"],
-        info["statistics"]["contents"]["children"]
+        info["statistics"]["contents"]["children"],
+        None #notes
     ])
 
     if "numAttachments" in info and info.get("numAttachments") > 0:
-        display_attachments (writer, info, tz)
+        display_attachments (writer, info, tz, 
+                             (info["created"]["date"], info["modified"]["date"], info["statistics"]["views"]["views"]), 
+                             (None, None, None))
     
     children = igloo.get_all_children_from_object(channelid)
     for article in children:
-        display_article (writer, article, tz)
+        display_article (writer, article, tz,
+                         (info["created"]["date"], info["modified"]["date"], info["statistics"]["views"]["views"]))
 
 def get_userinfo (user):
     if user:
@@ -129,33 +153,21 @@ def get_userinfo (user):
     else:
         return None
 
-def display_comment (writer, comment, tz):
-    writer.writerow([
-        '=HYPERLINK("' + os.getenv("API_ENDPOINT") + "/".join(comment["href"].split("/")[2:-1]) + "#anchor_" + comment["href"].split("/")[-1] + '","' + get_userinfo(comment["created"]) + ' replied")',
-        "/" + "/".join(comment["href"].split("/")[2:-1]) + "#anchor_" + comment["href"].split("/")[-1],
-        pyigloo.igloodates.date(comment["created"]["date"], tz).local,
-        pyigloo.igloodates.date(comment["modified"]["date"], tz).local,
-        pyigloo.iglootypes.types(comment).__str__(),
-        None,
-        get_userinfo(comment["created"]),
-        get_userinfo(comment["modified"]),
-        None,
-        comment.get("version"),
-        comment.get("numAttachments"),
-        None,
-        None
-    ], indent=2, outline=2, collapse=True)
-
-def display_article (writer, article, tz):
+def display_article (writer, article, tz, channel_info):
     writer.writerow([
         '=HYPERLINK("' + os.getenv("API_ENDPOINT")[:-1] + article["href"] + '","' + article["title"] + '")',
         article["href"],
+        pyigloo.igloodates.date(channel_info[0], tz).local,
         pyigloo.igloodates.date(article["created"]["date"], tz).local,
+        None,
+        pyigloo.igloodates.date(channel_info[1], tz).local,
         pyigloo.igloodates.date(article["modified"]["date"], tz).local,
+        None,
         pyigloo.iglootypes.types(article).__str__(),
         article["IsArchived"] and article["isPublished"],
         get_userinfo(article["created"]),
         get_userinfo(article["modified"]),
+        channel_info[2],
         article["statistics"]["views"]["views"],
         article.get("version"),
         article.get("numAttachments"),
@@ -163,26 +175,61 @@ def display_article (writer, article, tz):
         article["statistics"]["contents"]["children"] + int(article.get("numAttachments", 0))
     ], indent=1, outline=1)
     if "numAttachments" in article and article.get("numAttachments") > 0:
-        display_attachments (writer, article, tz)
+        display_attachments (writer, article, tz,
+                             channel_info,
+                             (article["created"]["date"], article["modified"]["date"], article["statistics"]["views"]["views"])
+                             )
     if article["statistics"]["contents"]["comments"] > 0:
         comments = igloo.get_all_comments_from_object(article["id"])
         for comment in comments:
-            display_comment (writer, comment, tz)
+            display_comment (writer, comment, tz, 
+                             channel_info,
+                             (article["created"]["date"], article["modified"]["date"], article["statistics"]["views"]["views"])
+                             )
 
-def display_attachments (writer, article, tz):
+def display_comment (writer, comment, tz, channel_info, article_info):
+    writer.writerow([
+        '=HYPERLINK("' + os.getenv("API_ENDPOINT") + "/".join(comment["href"].split("/")[2:-1]) + "#anchor_" + comment["href"].split("/")[-1] + '","' + get_userinfo(comment["created"]) + ' replied")',
+        "/" + "/".join(comment["href"].split("/")[2:-1]) + "#anchor_" + comment["href"].split("/")[-1],
+        pyigloo.igloodates.date(channel_info[0], tz).local,
+        pyigloo.igloodates.date(article_info[0], tz).local,
+        pyigloo.igloodates.date(comment["created"]["date"], tz).local,
+        pyigloo.igloodates.date(channel_info[1], tz).local,
+        pyigloo.igloodates.date(article_info[1], tz).local,
+        pyigloo.igloodates.date(comment["modified"]["date"], tz).local,
+        pyigloo.iglootypes.types(comment).__str__(),
+        None,
+        get_userinfo(comment["created"]),
+        get_userinfo(comment["modified"]),
+        channel_info[2],
+        article_info[2],
+        comment.get("version"),
+        comment.get("numAttachments"),
+        None,
+        None
+    ], indent=2, outline=2, collapse=True)
+
+def display_attachments (writer, article, tz, channel_info, article_info):
     attachments = igloo.attachments_view(article["id"])
     for attachment in attachments["items"]:
-        display_attachment (writer, article, attachment, tz)
+        display_attachment (writer, article, attachment, tz, channel_info, article_info)
 
-def display_attachment (writer, article, attachment, tz, last=False):
+def display_attachment (writer, article, attachment, tz, channel_info, article_info):
     writer.writerow([
         '=HYPERLINK("' + os.getenv("API_ENDPOINT")[:-1] + attachment["RelationHref"] + '","' + attachment["RelationTitle"] + '")',
         article["href"],
+        pyigloo.igloodates.date(channel_info[0], tz).local,
+        pyigloo.igloodates.date(article_info[0], tz).local,
         pyigloo.igloodates.date(attachment["Created"], tz).local,
+        pyigloo.igloodates.date(channel_info[1], tz).local,
+        pyigloo.igloodates.date(article_info[1], tz).local,
         None,
         pyigloo.iglootypes.types(attachment).__str__(),
         attachment["IsHidden"],
         get_userinfo(attachment["CreatedBy"]),
+        None,
+        channel_info[2],
+        article_info[2],
         None,
         None,
         None,
@@ -205,6 +252,7 @@ parser.add_argument("uri", help="Partial URL (URI) path of wiki channel")
 parser.add_argument('-w', '--writefile', type=argparse.FileType('w'), default='-', help="Filename to write into")
 parser.add_argument('-x', '--excel', help="Write output as specified Excel file")
 parser.add_argument('-t', '--timezone', type=TypeZone(), default=localtz, help="Specify a timezone for date/time objects")
+parser.add_argument('-g', '--group', help="Use row groups in Excel output. May break sorting operations.", action="store_true")
 parser.add_argument("-v", "--verbose", help="Be more verbose in output", action="store_true")
 args = parser.parse_args()
 
@@ -220,17 +268,22 @@ else:
 
 csvwriter = csv.writer(args.writefile, quoting=csv.QUOTE_NONNUMERIC)
 
-writer = file_writer (workbook, csvwriter)
+writer = file_writer (workbook, csvwriter, doGroup=args.group)
 
 writer.writerow(["Title",
                 "URI",
-                "Created",
-                "Last Updated",
+                "Created (channel)",
+                "Created (article)",
+                "Created (comment)",
+                "Updated (channel)",
+                "Updated (article)",
+                "Updated (comment)",
                 "Content type",
                 "Hidden?",
                 "Creator",
                 "Modifier",
-                "Total views",
+                "Views (channel)",
+                "Views (article)",
                 "Version",
                 "Attachments",
                 "Comments",
@@ -241,7 +294,7 @@ if mytype.info["type"] == "channel":
     display_channel (writer, root["id"], args.timezone)
 elif mytype.info["type"] == "article":
     article = igloo.objects_view(root["id"])
-    display_article (writer, article, args.timezone)
+    display_article (writer, article, args.timezone, (None, None, None))
 elif mytype.info["type"] == "container":
     display_container (writer, root["id"], args.timezone)
 
